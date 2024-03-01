31e26e7264116bc5d2171e5ce45adf95002056ca
efgarro <efgarro@yahoo.com>
2024-02-29 21:30:08 -0600

    0229b

--- 21-CreateApp-JS/src/App.js
+++ 21-CreateApp-JS/src/App.js
@@ -1,21 +1,15 @@
-
 import React from "react";
 
 import sty from "./sty.module.css";
 
-console.log(sty)
+console.log(sty);
 
-class App extends React.Component {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1 className={sty.h1class}>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
-}
+const App = ({ name }) => {
+  return (
+    <>
+      <h1 className={sty.h1class}>Hello {name}</h1>
+    </>
+  );
+};
 
 export default App;
