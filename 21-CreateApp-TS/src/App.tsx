efcbc1b3ff678175a9871c91c25d11da9ffb644e
efgarro <efgarro@yahoo.com>
2024-03-02 22:00:01 -0600

    ServerApp Template

--- 21-CreateApp-TS/src/App.tsx
+++ 21-CreateApp-TS/src/App.tsx
@@ -1,24 +1,12 @@
-
 import * as React from 'react';
 import sty from "./sty.module.css";
 
-
-interface Props {
-   name:
-    string
-}
-
-class App extends React.Component<Props> {
-  render() {
-    const { name } = this.props;
-    return (
-      <>
-        <h1 className={sty.h1class}>
-          Hello {name}
-        </h1>
-      </>
-    );
-  }
-}
+const App = ({ name } : {name: string}) => {
+  return (
+    <>
+      <h1 className={sty.h1class}>Hello {name}</h1>
+    </>
+  );
+};
 
 export default App;
